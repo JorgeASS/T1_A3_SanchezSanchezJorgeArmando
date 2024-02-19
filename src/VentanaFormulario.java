211d1dba8fa9ecc3982cf3e529ebb7cf4416c56f
JorgeSanchez <jorge.ss@tecjerez.edu.mx>
2024-02-19 12:25:15 -0600

    ImplementacionDeVentana

--- src/VentanaFormulario.java
+++ src/VentanaFormulario.java
@@ -1,9 +1,38 @@
 
+import java.awt.*;
+import javax.swing.*;
+
+class Formulario extends JFrame{
+	
+	
+	public Formulario() {
+		
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(750, 600);
+		setLocationRelativeTo(null);
+		setTitle("Formulario");
+		setVisible(true);
+		
+	}
+	
+	
+}
+
 public class VentanaFormulario {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new Formulario();
+			}
+		});
+		
 	}
 
 }
