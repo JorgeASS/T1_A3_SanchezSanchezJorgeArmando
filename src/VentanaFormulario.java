16f15a632d5af122c76bba1e84d6923683b929ba
JorgeSanchez <jorge.ss@tecjerez.edu.mx>
2024-02-25 19:18:12 -0600

    Actualizacion

--- src/VentanaFormulario.java
+++ src/VentanaFormulario.java
@@ -4,18 +4,46 @@ import javax.swing.*;
 
 class Formulario extends JFrame{
 	
+	GridBagLayout gbl = new GridBagLayout();
+	GridBagConstraints gbc = new GridBagConstraints();
 	
 	public Formulario() {
 		
-		getContentPane().setLayout(null);
+		//------------------------------Implementacion de ventana----------------------------------------
+		getContentPane().setLayout(gbl);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(750, 600);
-		setLocationRelativeTo(null);
-		setTitle("Formulario");
+		setTitle("GridBagLayout");
 		setVisible(true);
+		//------------------------------------------------------------------------------------------------
+		
+		
+		//------------------------------Inicio de codigo----------------------------------------
+		
+		
+		
+		//--------------------------------------------------------------------------------------
+		
+		//-----------------------------------------Complemento de ventana---------------------------------
+		
+		pack();
+		setLocationRelativeTo(null);
+		
+		//------------------------------------------------------------------------------------------------
 		
 	}
 	
+	public void bgcGrid (JComponent c, int x, int y, int w, int h) {
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridwidth = w;
+		gbc.gridheight = h;
+		gbl.setConstraints(c, gbc);
+		add(c);
+		
+	}
 	
 }
 
